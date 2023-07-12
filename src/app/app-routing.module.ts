e43dd343f002efa34f885845accf382b42e505e5
LuisGonzalez558 <a1120250002@utch.edu.mx>
2023-07-12 15:28:55 -0600

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'products', component: ProductsComponent},
 ];
 
 @NgModule({
